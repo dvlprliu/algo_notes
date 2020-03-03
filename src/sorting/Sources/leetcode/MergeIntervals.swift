4bae285dbed94e7612439c9d3f000678b60e9f6e
liuzhzh <liuzhzh@etutech.com>
2020-03-03 14:50:29 +0800

    fix merge interval solution
    修复针对 [[2,3],[4,5],[6,7],[8,9],[1,10]] 的bug。实现方式从“一次找到所有可合并的
    区间”调整为，逐个合并， 检查i和i+1是否可以合并，如果可以合并，得到新的区间，再用
    该区间去和i+2进行比较。迭代此过程。

--- src/sorting/Sources/leetcode/MergeIntervals.swift
+++ src/sorting/Sources/leetcode/MergeIntervals.swift
@@ -10,19 +10,17 @@ extension Solution {
         let intervals = intervals.sorted { $0[0] < $1[0] }
         var i = 0
         var result = [[Int]]()
-        while i <= intervals.count - 1 {
+        while i < intervals.count {
             var j = i
+            var merged = intervals[i]
             while j < intervals.count - 1 {
-                if intervals[j][1] >= intervals[j+1][0] {
+                if merged[1] >= intervals[j+1][0] {
+                    merged = [merged[0], max(merged[1], intervals[j+1][1])]
                     j += 1
                 } else {
                     break
                 }
             }
-            // 合并
-            let lower = min(intervals[i][0], intervals[j][0])
-            let upper = max(intervals[i][1], intervals[j][1])
-            let merged = [lower, upper]
             result.append(merged)
             i = j + 1
         }
