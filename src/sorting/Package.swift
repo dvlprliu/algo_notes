cbd40ddb1186a4e99f09c604b0a0d3494bbd03e2
liuzhzh <liuzhzh@etutech.com>
2020-03-03 16:34:23 +0800

    update project structure

--- src/sorting/Package.swift
+++ src/sorting/Package.swift
@@ -6,9 +6,6 @@ import PackageDescription
 let package = Package(
     name: "sorting",
     products: [
-        .library(
-            name: "leetcode", 
-            targets: ["leetcode"]),
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "sorting",
@@ -19,12 +16,14 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "leetcode",
-                dependencies: []),
+
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "sorting",
+            dependencies: []),
+        .target(
+            name: "leetcode",
             dependencies: []),
         .testTarget(
             name: "sortingTests",
