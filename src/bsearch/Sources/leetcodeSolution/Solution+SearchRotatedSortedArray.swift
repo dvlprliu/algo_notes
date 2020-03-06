fe7c58ff219b86dffaf5ee79477e724f27578f55
liuzhzh <liuzhzh@etutech.com>
2020-03-06 17:45:00 +0800

    update search rotated sorted array solution AC
    可以直接使用二分查找进行求解，需要根据 nums[low] nums[mid] nums[high]之间的关系
    来确定转轴的问题，当nums[low] <= nums[mid]的时候，说明左边是单调递增的，此时判断
    目标元素是否在[low, mid]区间中，如果在，就在该区间进行查找，如果不在，就去[mid +
    1, high]区间中进行查找。

--- src/bsearch/Sources/leetcodeSolution/Solution+SearchRotatedSortedArray.swift
+++ src/bsearch/Sources/leetcodeSolution/Solution+SearchRotatedSortedArray.swift
@@ -24,34 +24,38 @@ import Foundation
 
 extension Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        if nums.isEmpty { return -1 }
-        // find retation point
-        var p = 0
-        for i in 0 ..< nums.count - 1 where nums[i] > nums[i + 1] {
-            p = i
-            break
-        }
-        var low = -1
-        var high = -1
-        if p == 0 {
-            low = 0
-            high = nums.count - 1
-        } else if target <= nums[nums.count - 1] {
-            low = p + 1
-            high = nums.count - 1
-        } else {
-            low = 0
-            high = p
-        }
-        while low < high {
+        var low = 0
+        var high = nums.count - 1
+        while low <= high {
             let mid = low + (high - low) / 2
-            if nums[mid] < target {
-                low = mid + 1
-            } else {
-                high = mid
+            if nums[mid] == target {
+                return mid
+            }
+            // [low, mid]是单调递增的区间，说明转轴在mid右边
+            if nums[low] <= nums[mid] {
+                // 如果[low, mid]包含要查找的元素，则将查找范围缩小到
+                // [low, mid - 1]之间
+                if target < nums[mid] && target >= nums[low] {
+                    high = mid - 1
+                }
+                // 到mid右边去找
+                else {
+                    low = mid + 1
+                }
+            }
+            // [mid, high] 之间是单调递增的，说明转轴在mid左边
+            else {
+                // 如果[mid, high]之间包含目标元素，则将查找范围缩小
+                // 到[mid + 1, high]之间
+                if target > nums[mid] && target <= nums[high] {
+                    low = mid + 1
+                }
+                // 到mid右边去找
+                else {
+                    high = mid - 1
+                }
             }
         }
-
-        return nums[low] == target ? low : -1
+        return -1
     }
 }
