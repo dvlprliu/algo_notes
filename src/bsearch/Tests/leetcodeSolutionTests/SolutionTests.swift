efed52a200126acac081e3abd69c15b0fd1851ca
liuzhzh <liuzhzh@etutech.com>
2020-03-06 18:23:58 +0800

    add seach matrix solution AC
    使用swift高阶函数实现，时间复杂度 O(m * n)

--- src/bsearch/Tests/leetcodeSolutionTests/SolutionTests.swift
+++ src/bsearch/Tests/leetcodeSolutionTests/SolutionTests.swift
@@ -5,27 +5,23 @@
 //  Created by zhzh liu on 3/6/20.
 //
 
-import XCTest
 @testable import leetcodeSolution
+import XCTest
 
 final class SolutionTests: XCTestCase {
-    override func setUp() {
+    override func setUp() {}
 
-    }
-
-    override class func tearDown() {
-
-    }
+    override class func tearDown() {}
 
     func testSearchInsertPosition() {
         let cases = [
-            ([1,3,5,6], 5, 2),
-            ([1,3,5,6], 2, 1),
-            ([1,3,5,6], 7, 4),
-            ([1,3,5,6], 0, 0)
+            ([1, 3, 5, 6], 5, 2),
+            ([1, 3, 5, 6], 2, 1),
+            ([1, 3, 5, 6], 7, 4),
+            ([1, 3, 5, 6], 0, 0)
         ]
         let solution = Solution()
-        cases.forEach { (`case`) in
+        cases.forEach { `case` in
             let (nums, target, ans) = `case`
             let p = solution.searchInsert(nums, target)
             XCTAssertEqual(p, ans)
@@ -47,7 +43,7 @@ final class SolutionTests: XCTestCase {
             return (num, ans)
         }
         let solution = Solution()
-        (cases + randomTest).forEach { (case) in
+        (cases + randomTest).forEach { `case` in
             let (x, n) = `case`
             XCTAssertEqual(solution.mySqrt(x), n)
         }
@@ -55,21 +51,49 @@ final class SolutionTests: XCTestCase {
 
     func testSearchRotatedSortedArray() {
         let cases = [
-            ([4,5,6,7,0,1,2], 0, 4),
-            ([4,5,6,7,0,1,2], 3, -1),
-            ([6,7,0,1,2,4,5], 2, 4),
-            ([6,7,0,1,2,4,5], 8, -1),
-            ([0,1,2,4,5,6,7], 2, 2),
-            ([6,7,0,1,2,4,5], 3, -1),
-            ([3,1], 1, 1)
+            ([4, 5, 6, 7, 0, 1, 2], 0, 4),
+            ([4, 5, 6, 7, 0, 1, 2], 3, -1),
+            ([6, 7, 0, 1, 2, 4, 5], 2, 4),
+            ([6, 7, 0, 1, 2, 4, 5], 8, -1),
+            ([0, 1, 2, 4, 5, 6, 7], 2, 2),
+            ([6, 7, 0, 1, 2, 4, 5], 3, -1),
+            ([3, 1], 1, 1)
         ]
         let solution = Solution()
-        cases.forEach { (case) in
+        cases.forEach { `case` in
             let (nums, target, ans) = `case`
             let result = solution.search(nums, target)
             XCTAssertEqual(result, ans, "searching \(target) in \(nums) should be \(ans) but get \(result)")
         }
     }
+
+    func testSearchMatrix() {
+        let cases = [
+            ([
+                [1, 3, 5, 7],
+                [10, 11, 16, 20],
+                [23, 30, 34, 50]
+            ], 3, true),
+            ([
+                [1, 3, 5, 7],
+                [10, 11, 16, 20],
+                [23, 30, 34, 50]
+            ], 13, false),
+            ([
+                [1], [3], [5]
+            ], 5, true),
+            ([
+                [1], [3]
+            ], 3, true),
+            ([
+                []
+            ], 3, false)
+        ]
+
+        let solution = Solution()
+        cases.forEach { `case` in
+            let (matrix, target, ans) = `case`
+            XCTAssertEqual(solution.searchMatrix(matrix, target), ans)
+        }
+    }
 }
-
-
