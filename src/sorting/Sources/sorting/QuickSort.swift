86ae537a954986f7e8fa8852bd33f31a40c9f03a
liuzhzh <liuzhzh@etutech.com>
2020-04-01 09:21:41 +0800

    add new partition method to quicksort

--- src/sorting/Sources/sorting/QuickSort.swift
+++ src/sorting/Sources/sorting/QuickSort.swift
@@ -8,12 +8,12 @@ extension Sort {
 
     private func quickSort(_ array: inout [T], low: Int, hi: Int) {
         guard low < hi else { return }
-        let p = partition(&array, low: low, hi: hi)
+        let p = partition_low(&array, low: low, high: hi)
         quickSort(&array, low: low, hi: p - 1)
         quickSort(&array, low: p + 1, hi: hi)
     }
 
-    private func partition(_ array: inout [T], low: Int, hi: Int) -> Int {
+    private func partition_high(_ array: inout [T], low: Int, hi: Int) -> Int {
         let pivot = array[hi]
         var i = low
         for j in low ..< hi where array[j] <= pivot {
@@ -22,5 +22,24 @@ extension Sort {
         }
         array.swapAt(i, hi)
         return i
+    }
+
+    private func partition_low(_ array: inout [T], low: Int, high: Int) -> Int {
+        var left = low
+        var right = high
+        let pivot = array[low]
+        while left < right {
+            while left < right, array[right] >= pivot {
+                right -= 1
+            }
+            array[left] = array[right]
+
+            while left < right, array[left] < pivot {
+                left += 1
+            }
+            array[right] = array[left]
+        }
+        array[left] = pivot
+        return left
     }
 }
