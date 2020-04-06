79454ae1748fcf8c6665752119ba4b2ad959687d
liuzhzh <liuzhzh@etutech.com>
2020-04-06 17:32:29 +0800

    add PriorityQueue implementation

---
+++ src/heap/Sources/heap/PriorityQueue.swift
@@ -0,0 +1,41 @@
+//
+//  PriorityQueue.swift
+//  heap
+//
+//  Created by zhzh liu on 4/6/20.
+//
+//  Credit: SwiftAlgorithmClub
+
+import Foundation
+
+public struct PriorityQueue<T> {
+    private(set) var heap: Heap<T>
+
+    init(sort: @escaping (T, T) -> Bool) {
+        self.heap = Heap<T>(sort: sort)
+    }
+
+    public var count: Int {
+        heap.count
+    }
+
+    public var isEmpty: Bool {
+        heap.isEmpty
+    }
+
+    public func peek() -> T? {
+        heap.peek()
+    }
+
+    public mutating func enqueue(_ value: T) {
+        heap.insert(value)
+    }
+
+    public mutating func dequeue() -> T? {
+        heap.remove()
+    }
+
+    public mutating func changePriority(index: Int, value: T) {
+        heap.replace(index: index, value: value)
+    }
+}
---
+++ src/heap/Tests/heapTests/PriorityQueueTests.swift
@@ -0,0 +1,89 @@
+//
+//  PriorityQueueTests.swift
+//  heapTests
+//
+//  Created by zhzh liu on 4/6/20.
+//
+//  Credit:  SwiftAlgorithmClub
+
+import XCTest
+@testable import heap
+
+private struct Message {
+    let text: String
+    let priority: Int
+}
+
+private func <(lhs: Message, rhs: Message) -> Bool {
+    return lhs.priority < rhs.priority
+}
+
+class PriorityQueueTests: XCTestCase {
+
+    func testEmpty() {
+        var queue = PriorityQueue<Message>(sort: <)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertNil(queue.peek())
+        XCTAssertNil(queue.dequeue())
+    }
+
+    func testOneElement() {
+        var queue = PriorityQueue<Message>(sort: <)
+
+        queue.enqueue(Message(text: "Hello", priority: 100))
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 1)
+        XCTAssertEqual(queue.peek()!.priority, 100)
+
+        let result = queue.dequeue()
+        XCTAssertEqual(result!.priority, 100)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertNil(queue.peek())
+    }
+
+    func testTwoElementsInOrder() {
+        var queue = PriorityQueue<Message>(sort: <)
+
+        queue.enqueue(Message(text: "hello", priority: 100))
+        queue.enqueue(Message(text: "world", priority: 200))
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 2)
+        XCTAssertEqual(queue.peek()!.priority, 100)
+
+        let result1 = queue.dequeue()
+        XCTAssertEqual(result1!.priority, 100)
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 1)
+        XCTAssertEqual(queue.peek()!.priority, 200)
+
+        let result2 = queue.dequeue()
+        XCTAssertEqual(result2!.priority, 200)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertNil(queue.peek())
+    }
+
+    func testTwoElementsOutOfOrder() {
+        var queue = PriorityQueue<Message>(sort: <)
+
+        queue.enqueue(Message(text: "world", priority: 200))
+        queue.enqueue(Message(text: "hello", priority: 100))
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 2)
+        XCTAssertEqual(queue.peek()!.priority, 100)
+
+        let result1 = queue.dequeue()
+        XCTAssertEqual(result1!.priority, 100)
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 1)
+        XCTAssertEqual(queue.peek()!.priority, 200)
+
+        let result2 = queue.dequeue()
+        XCTAssertEqual(result2!.priority, 200)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertNil(queue.peek())
+    }
+}
