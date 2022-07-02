09a8243a7689e0f6613beaf140f0f61a61a191c8
liuzhzh <dvlprliu@gmail.com>
2022-07-02 19:37:07 +0800

    add mergeList solutions

--- src/linked-list/Sources/linked-list/Solution.swift
+++ src/linked-list/Sources/linked-list/Solution.swift
@@ -81,4 +81,53 @@ class Solution {
             return ret
         }
     }
+
+    enum MergeTwoLists {
+        typealias Solution = (ListNode?, ListNode?) -> ListNode?
+        static func byIterate(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+            if l1 == nil { return l2 }
+            if l2 == nil { return l1 }
+            var newList: ListNode? = nil
+            var head = newList
+            var l1N = l1
+            var l2N = l2
+            while let l = l1N, let r = l2N {
+                var lower: ListNode? = nil
+                if l.val <= r.val {
+                    lower = l
+                    l1N = l1N?.next
+                } else {
+                    lower = r
+                    l2N = l2N?.next
+                }
+                if newList == nil {
+                    newList = lower
+                    head = lower
+                } else {
+                    newList?.next = lower
+                    newList = lower
+                }
+            }
+            if l1N != nil {
+                newList?.next = l1N
+            } else {
+                newList?.next = l2N
+            }
+            return head
+        }
+
+        static func byRecursive(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+            if l1 == nil {
+                return l2
+            } else if l2 == nil {
+                return l1
+            } else if l1!.val <= l2!.val {
+                l1?.next = byRecursive(l1?.next, l2)
+                return l1
+            } else {
+                l2?.next = byRecursive(l1, l2?.next)
+                return l2
+            }
+        }
+    }
 }
